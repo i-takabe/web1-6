b97c5e4adce14cff1d9ad825db08051c53f62e97
Unknown <g1827402@st.t-kougei.ac.jp>
2018-06-04 17:46:42 +0900

    prefabと当たり判定

---
+++ New Unity Project/Assets/ArrowController.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ArrowController : MonoBehaviour {
+
+    GameObject player;
+
+	// Use this for initialization
+	void Start () {
+        this.player = GameObject.Find("player");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.Translate(0, -0.1f, 0);
+        if(transform.position.y < -5.0f)
+        {
+            Destroy(gameObject);
+        }
+
+        Vector2 arrowCollision = transform.position;
+        Vector2 playerCollision = this.player.transform.position;
+        Vector2 dir = arrowCollision - playerCollision;
+
+        float d = dir.magnitude;
+        float arrowCollisonSize = 0.5f;
+        float playerCollisonSize = 1.0f;
+
+        if(d < arrowCollisonSize + playerCollisonSize)
+        {
+
+            GameObject director = GameObject.Find("GameDirector");
+            director.GetComponent<GameDirector>().DecreaseHp();
+            Destroy(gameObject);
+        }
+    }
+}
